eba784f21e4d06da438435cd60d07b92ccc9ab4c
Rafael Reis <rafael.reis.a@gmail.com>
2023-06-26 09:33:56 -0300

    feat: add unit of work

---
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF;
+public class UnitOfWork
+    : IUnitOfWork
+{
+    private readonly CodeflixCatalogDbContext _context;
+
+    public UnitOfWork(CodeflixCatalogDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task Commit(CancellationToken cancellationToken)
+    {
+        return _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public Task Rollback(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/UnitOfWork/UnitOfWorkTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using UnitOfWorkInfra = FC.Codeflix.Catalog.Infra.Data.EF;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.UnitOfWork;
+
+[Collection(nameof(UnitOfWorkTestFixture))]
+public class UnitOfWorkTest
+{
+    private readonly UnitOfWorkTestFixture _fixture;
+
+    public UnitOfWorkTest(UnitOfWorkTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Commit))]
+    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
+    public async Task Commit()
+    {
+        var dbContext = _fixture.GetDbContext(false);
+        var categoriesList = _fixture.GetCategoriesList();
+        await dbContext.AddRangeAsync(categoriesList);
+        var unitOfWork = new UnitOfWorkInfra.UnitOfWork(dbContext);
+
+        await unitOfWork.Commit(CancellationToken.None);
+
+        var assertDbContext = _fixture.GetDbContext(true);
+        var savedCategories = assertDbContext.Categories
+            .AsNoTracking()
+            .ToList();
+
+        savedCategories.Should().HaveCount(categoriesList.Count);
+
+    }
+
+    [Fact(DisplayName = nameof(Rollback))]
+    [Trait("Integration/Infra.Data", "UnitOfWork - Persistence")]
+    public async Task Rollback()
+    {
+        var dbContext = _fixture.GetDbContext(false);
+        var unitOfWork = new UnitOfWorkInfra.UnitOfWork(dbContext);
+
+        var task = async () => await unitOfWork.Rollback(CancellationToken.None);
+
+        await task.Should().NotThrowAsync();
+    }
+}
