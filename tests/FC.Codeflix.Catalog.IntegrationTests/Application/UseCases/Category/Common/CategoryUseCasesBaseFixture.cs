a7dd86b36c03a3581e097cf9992305ac11c90169
Rafael Reis <rafael.reis.a@gmail.com>
2023-06-27 07:33:22 -0300

    test: get category integration tests

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/Common/CategoryUseCasesBaseFixture.cs
@@ -0,0 +1,42 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Base;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+public class CategoryUseCasesBaseFixture
+    : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10000)
+            categoryDescription = categoryDescription[..10000];
+        return categoryDescription;
+    }
+
+    public bool getRandomBoolean()
+        => new Random().NextDouble() < 0.5;
+
+    public DomainEntity.Category GetValidCategory()
+    => new(
+        GetValidCategoryName(),
+        GetValidCategoryDescription(),
+        getRandomBoolean()
+        );
+
+    public List<DomainEntity.Category> GetCategoriesList(int lenght = 10)
+    {
+        return Enumerable.Range(1, lenght)
+            .Select(_ => GetValidCategory()
+        ).ToList();
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+using FC.Codeflix.Catalog.IntegrationTests.Base;
+using Xunit;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.GetCategory;
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureColletion
+    : ICollectionFixture<GetCategoryTestFixture>
+{ }
+
+public class GetCategoryTestFixture 
+    : CategoryUseCasesBaseFixture
+{
+}
