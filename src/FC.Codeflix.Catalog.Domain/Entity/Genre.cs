eca3ee2ce601449691ef41d475c42363c5f8fdb7
Rafael Reis <rafael.reis.a@gmail.com>
2023-09-01 14:47:49 -0300

    feat: add genre domain entity

---
+++ src/FC.Codeflix.Catalog.Domain/Entity/Genre.cs
@@ -0,0 +1,18 @@
+﻿namespace FC.Codeflix.Catalog.Domain.Entity;
+public class Genre
+{
+    public Genre(
+        string name, 
+        bool isActive = true
+    )
+    {
+        Name = name;
+        IsActive = isActive;
+        CreatedAt = DateTime.Now;
+    }
+
+    public string Name { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/FC.Codeflix.Catalog.UnitTests/Domain/Entity/Genre/GenreTest.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Xunit;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entity;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entity.Genre;
+
+[Collection(nameof(GenreTestFixture))]
+public class GenreTest
+{
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Genre - Aggregates")]
+    public void Instantiate()
+    {
+        var genrerName = "Horror";
+        var datetimeBefore = DateTime.Now;
+
+        var genre = new DomainEntity.Genre(genrerName);
+        var datetimeAfter = DateTime.Now.AddSeconds(1);
+
+        genre.Should().NotBeNull();
+        genre.Name.Should().Be(genrerName);
+        genre.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+        genre.CreatedAt.Should().BeOnOrAfter(datetimeBefore);
+        genre.CreatedAt.Should().BeOnOrBefore(datetimeAfter);
+        genre.IsActive.Should().BeTrue();
+    }
+
+    [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+    [Trait("Domain", "Genre - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateWithIsActive(bool isActive)
+    {
+        var genrerName = "Horror";
+        var datetimeBefore = DateTime.Now;
+
+        var genre = new DomainEntity.Genre(genrerName, isActive);
+        var datetimeAfter = DateTime.Now.AddSeconds(1);
+
+        genre.Should().NotBeNull();
+        genre.Name.Should().Be(genrerName);
+        genre.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+        genre.CreatedAt.Should().BeOnOrAfter(datetimeBefore);
+        genre.CreatedAt.Should().BeOnOrBefore(datetimeAfter);
+        genre.IsActive.Should().Be(isActive);
+    }
+
+}
